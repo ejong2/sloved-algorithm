a17b7c4806b7a8b3f66e1354f5bef2ff3439c9a4
enamu <guyus5594@gmail.com>
2023-01-05 08:38:25 +0900

    백준 11660 구간 합 구하기 5

--- Algo/main.cpp
+++ Algo/main.cpp
@@ -1,35 +1,39 @@
-// 백준 11441 합 구하기
+// 백준 11660 구간 합 구하기 5
 
 #include <iostream>
 
-#define MAX 100001
+#define MAX 1025
 
-using namespace std;
-
-int Arr[MAX];
+int DP[MAX][MAX];
+int MAP[MAX][MAX];
 
 int main()
 {
-	int T;
-	cin >> T;
-	for (int i = 0; i < T; i++)
-	{
-		int a;
-		scanf_s("%d", &a);
+	int N, M;
+	scanf("%d %d", &N, &M);
 
-		Arr[i + 1] = Arr[i] + a;
+
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N; j++)
+		{
+
+			scanf("%d", &MAP[i][j]);
+			DP[i][j] = DP[i - 1][j] + DP[i][j - 1] - DP[i - 1][j - 1] + MAP[i][j];
+		}
 	}
+	
+	int x1, x2, y1, y2;
+	int ans;
 
-	int N;
-	cin >> N;
-
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < M; i++)
 	{
-		int b, c;
-		scanf_s("%d", &b);
-		scanf_s("%d", &c);
+		scanf("%d %d", &x1, &y1);
+		scanf("%d %d", &x2, &y2);
 
-		printf("%d\n", Arr[c] - Arr[b - 1]);
+		ans = DP[x2][y2] - DP[x1 - 1][y2] - DP[x2][y1 - 1] + DP[x1 - 1][y1 - 1];
+
+		printf("%d\n", ans);
 	}
 
 	return 0;
