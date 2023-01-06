12953a8c3774503eaaf381c6b188fe84aad58ddc
enamu <guyus5594@gmail.com>
2023-01-06 21:33:01 +0900

    재귀함수를 사용한 연결리스트 구현

--- Algo/main.cpp
+++ Algo/main.cpp
@@ -1,30 +1,95 @@
-// 문자열 뒤집기 함수
-
 #include <iostream>
 
-#define MAX 101
+#define MAX 10
 
 using namespace std;
 
-void ReverseString(char* str)
+typedef struct Node
 {
-	int n = strlen(str);
+    int data;
+    struct Node* Next;
 
-	for (int i = 0; i < n / 2; i++)
-	{
-		int tmp = str[i];
-		str[i] = str[n - i - 1];
-		str[n - i - 1] = tmp;
-	}
-	cout << str;
+}    Node;
+
+Node* Insert(Node* node, int data) // 데이터 추가
+{
+    if (node == NULL)
+    {
+        Node* newNode = new Node();
+        newNode->data = data;
+        newNode->Next = NULL;
+
+        return newNode;
+    }
+    node->Next = Insert(node->Next, data);
+    return node;
+}
+
+Node* Search(Node* node, int data) // 데이터 탐색
+{
+    if (node == NULL)
+    {
+        return NULL;
+    }
+    if (node->data == data)
+    {
+        return node;
+    }
+    return Search(node->Next, data);
+}
+
+Node* Delete(Node* node, int data) // 데이터 삭제
+{
+    if (node == NULL)
+    {
+        return NULL;
+    }
+    if (node->data == data)
+    {
+        Node* nextNode = node->Next;
+        free(node);
+
+        return nextNode;
+    }
+    node->Next = Delete(node->Next, data);
+    return node;
+}
+
+void Print(Node* node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+
+    cout << node->data;
+    Print(node->Next);
 }
 
 int main()
 {
-	char c[MAX];
-	cin >> c;
+    Node* head = NULL;
 
-	ReverseString(c);
+    // 데이터 추가
+    for (int i = 0; i < MAX; i++)
+    {
+        head = Insert(head, i);
+    }
+    cout << "결과 : ";
+    Print(head);
 
-	return 0;
+    cout << '\n';
+
+    cout << "결과 : ";
+    Node* someNode = Search(head, 5);
+    if (someNode != NULL)
+    {
+        cout << someNode->data << '\n';
+    }
+    
+    cout << "결과 : ";
+    head = Delete(head, 4);
+    Print(head);
+
+    return 0;
 }
