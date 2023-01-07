9b80a283133299d342fce9da1cb04676e3dcf7ed
enamu <guyus5594@gmail.com>
2023-01-07 19:12:57 +0900

    백준 1436 영화감독 숌

--- Algo/main.cpp
+++ Algo/main.cpp
@@ -1,95 +1,36 @@
-#include <iostream>
+// 백준 1436 영화감독 숌
 
-#define MAX 10
+#include <iostream>
 
 using namespace std;
 
-typedef struct Node
-{
-    int data;
-    struct Node* Next;
-
-}    Node;
-
-Node* Insert(Node* node, int data) // 데이터 추가
-{
-    if (node == NULL)
-    {
-        Node* newNode = new Node();
-        newNode->data = data;
-        newNode->Next = NULL;
-
-        return newNode;
-    }
-    node->Next = Insert(node->Next, data);
-    return node;
-}
-
-Node* Search(Node* node, int data) // 데이터 탐색
-{
-    if (node == NULL)
-    {
-        return NULL;
-    }
-    if (node->data == data)
-    {
-        return node;
-    }
-    return Search(node->Next, data);
-}
-
-Node* Delete(Node* node, int data) // 데이터 삭제
-{
-    if (node == NULL)
-    {
-        return NULL;
-    }
-    if (node->data == data)
-    {
-        Node* nextNode = node->Next;
-        free(node);
-
-        return nextNode;
-    }
-    node->Next = Delete(node->Next, data);
-    return node;
-}
-
-void Print(Node* node)
-{
-    if (node == NULL)
-    {
-        return;
-    }
-
-    cout << node->data;
-    Print(node->Next);
-}
-
 int main()
 {
-    Node* head = NULL;
+	int start = 666, cnt = 0, tmp;
+	int N;
+	cin >> N;
 
-    // 데이터 추가
-    for (int i = 0; i < MAX; i++)
-    {
-        head = Insert(head, i);
-    }
-    cout << "결과 : ";
-    Print(head);
+	while (1)
+	{
+		tmp = start;
+		do
+		{
+			if (tmp % 1000 == 666)
+			{
+				cnt++;
+				break;
+			}
+			tmp /= 10;
 
-    cout << '\n';
+		} while (tmp > 0);
 
-    cout << "결과 : ";
-    Node* someNode = Search(head, 5);
-    if (someNode != NULL)
-    {
-        cout << someNode->data << '\n';
-    }
-    
-    cout << "결과 : ";
-    head = Delete(head, 4);
-    Print(head);
+		if (cnt == N)
+		{
+			cout << start << '\n';
+			break;
+		}
+		start++;
+	}
 
-    return 0;
+	return 0;
 }
