99701e80830045c2615681a264a7c9823efb9daf
enamu <guyus5594@gmail.com>
2023-01-09 21:04:55 +0900

    BFS 트리 구현

--- Algo/main.cpp
+++ Algo/main.cpp
@@ -1,47 +1,64 @@
-#include <string>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <queue>
+#include <vector>
+
+#define MAX 7
 
 using namespace std;
 
-vector<int> arr(2010);
+int number[MAX];
+bool visited[MAX];
+vector<int> v[8];
 
-int solution(vector<int> array)
+void BFS(int x)
 {
-    int answer = 0;
-    for (int i = 0; i < array.size(); i++)
-    {
-        arr[array[i]]++;
-    }
+	queue<int> q;
+	q.push(x);
+	visited[x] = true;
+	
+	while (!q.empty())
+	{
+		int x = q.front();
+		q.pop();
 
-    int max = 0;
-    for (int i = 0; i < arr.size(); i++)
-    {
-        if (max < arr[i])
-            max = arr[i];
-    }
-
-    int cnt = 0;
-    for (int i = 0; i < arr.size(); i++)
-    {
-        if (max == arr[i])
-        {
-            answer = i;
-            cnt++;
-            if (cnt > 1)
-                return -1;
-        }
-    }
-
-    return answer;
+		cout << x << ' ';
+		
+		for (int i = 0; i < v[x].size(); i++)
+		{
+			int y = v[x][i];
+			if (!visited[y])
+			{
+				q.push(y);
+				visited[y] = true;
+			}
+		}
+	}
 }
 
 int main()
 {
-    vector<int> v = { 5, 3, 3, 5, 1 };
+	v[1].push_back(2);
+	v[2].push_back(1);
 
-    cout << solution(v);
-    
+	v[1].push_back(3);
+	v[3].push_back(1);
+
+	v[2].push_back(3);
+	v[3].push_back(2);
+
+	v[2].push_back(4);
+	v[4].push_back(2);
+
+	v[2].push_back(5);
+	v[5].push_back(2);
+
+	v[3].push_back(6);
+	v[6].push_back(3);
+
+	v[3].push_back(7);
+	v[7].push_back(3);
+
+	BFS(1);
+
 	return 0;
 }
