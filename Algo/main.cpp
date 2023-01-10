1551bbdb0d0edfc138bac7de7445e310cd9321bd
enamu <guyus5594@gmail.com>
2023-01-10 13:22:48 +0900

    서버 로직 정리

--- Algo/main.cpp
+++ Algo/main.cpp
@@ -1,66 +1,27 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-
-#define MAX 101
 
 using namespace std;
 
-int N, M;
-int MAP[MAX][MAX];
-int Cnt[MAX][MAX];
-bool visited[MAX][MAX];
-
-int dx[] = { 0, 0, -1, 1 };
-int dy[] = { -1, 1, 0, 0 };
-
-void BFS(int x, int y)
+void ReverseString(char* str)
 {
-	queue<pair<int, int>> q;
+	int n = strlen(str);
 
-	Cnt[x][y]++;
-	visited[x][y] = true;
-	q.push({ x, y });
-
-	while (!q.empty())
+	for (int i = 0; i < n / 2; i++)
 	{
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-
-		for (int i = 0; i < 4; i++)
-		{
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-
-			if (nx >= 0 && ny >= 0 && nx < N && ny < M)
-			{
-				if (MAP[nx][ny] == 1 && !visited[nx][ny])
-				{
-					Cnt[nx][ny] = Cnt[x][y] + 1;
-					visited[nx][ny] = true;
-					q.push(make_pair(nx, ny));
-				}
-			}
-		}
-
+		int tmp = str[i];
+		str[i] = str[n - i - 1];
+		str[n - i - 1] = tmp;
 	}
+	cout << str;
 }
 
 int main()
 {
-	cin >> N >> M;
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			scanf_s("%1d", &MAP[i][j]);
-		}
-	}
+	char Buffer[10];
+	
+	cin >> Buffer;
 
-	BFS(0, 0);
-
-	cout << Cnt[N - 1][M - 1] << '\n';
+	ReverseString(Buffer);
 
 	return 0;
 }
