133a230aed18f01073245f6c5dbac9d36c03cfcb
enamu <guyus5594@gmail.com>
2023-01-22 23:56:09 +0900

    프로그래머스

--- Algo/main.cpp
+++ Algo/main.cpp
@@ -1,42 +1,33 @@
+#include <string>
+#include <vector>
+#include <sstream>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int cnt = 1;
+int solution(int num, int k)
+{
+	int answer = 0;
+
+	string s = to_string(num);
+	string v = to_string(k);
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		answer = -1;
+
+		if (s[i] == v[0])
+		{
+			return answer = i + 1;
+		}		
+	}
+	return answer;
+}
+
 int main()
 {
-	int n;
-	cin >> n;
-
-	for (int i = 1; i <= n * 2 - 1; i++)
-	{
-		if (i <= n)
-		{
-			for (int j = n - 1; j >= i; j--)
-			{
-				cout << " ";
-			}
-
-			for (int j = 1; j <= i * 2 - 1; j++)
-			{
-				cout << "*";
-			}
-			cout << '\n';
-		}
-		else
-		{
-			for (int j = 1; j <= i - n; j++)
-			{
-				cout << " ";
-			}
-			for (int j = (n - 1) * 2 - (cnt * 2 - 1); j > 0; j--)
-			{
-				cout << "*";
-			}
-			cout << '\n';
-			cnt++;
-		}
-	}
+	cout << solution(29183, 1);
 
 	return 0;
 }
