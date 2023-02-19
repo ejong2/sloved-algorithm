6ede7c72b1ebf94ccba62469d07e57caefae44c2
enamu <guyus5594@gmail.com>
2023-02-19 21:45:31 +0900

    가장 많은 숫자

--- Algo/main.cpp
+++ Algo/main.cpp
@@ -1,23 +1,22 @@
-#include <string>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int solution(vector<int> v)
-{
-    if (v[0] < v[1])
-        swap(v[0], v[1]);
-    return v[1] * 2 - 1;
-}
-
-
 int main()
 {
-    string s = "aAb1B2cC34oOp";
+	int N;
+	cin >> N;
 
-    cout << solution();
+	for (int i = 0; i < N; i++)
+	{
+		int a;
+		cin >> a;
+		for (int j = 0; j < a; j++)
+		{
+			cout << "=";
+		}
+		cout << '\n';
+	}
 
-    return 0;
+	return 0;
 }
