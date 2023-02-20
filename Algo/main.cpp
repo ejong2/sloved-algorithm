a7b9219b8d97663ec5ac768bbcfce1c92cab65ef
enamu <guyus5594@gmail.com>
2023-02-20 20:14:29 +0900

    체스판

--- Algo/main.cpp
+++ Algo/main.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
+int main(void)
 {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 	int N;
 	cin >> N;
 
-	for (int i = 0; i < N; i++)
-	{
-		int a;
-		cin >> a;
-		for (int j = 0; j < a; j++)
-		{
-			cout << "=";
-		}
-		cout << '\n';
-	}
+	int row = N / 2 + 1;
+	int col = N - row + 2;
+	int result = row * col;
+
+	cout << result << "\n";
 
 	return 0;
 }
