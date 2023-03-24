f4c1a64cc48061bbeba5b5e11ec58c579faae4ed
enamu <guyus5594@gmail.com>
2023-03-24 21:13:33 +0900

    팰린드롬, 리버스 스트링

--- Algo/main.cpp
+++ Algo/main.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 
-using namespace std;
+bool is_palindrome(int num)
+{
+    std::string str_num = std::to_string(num);
+    std::string reversed_num = str_num;
+    std::reverse(reversed_num.begin(), reversed_num.end());
+    return str_num == reversed_num;
+}
+
+int reverse_number(int num)
+{
+    std::string str_num = std::to_string(num);
+    std::reverse(str_num.begin(), str_num.end());
+    return std::stoi(str_num);
+}
 
 int main()
 {
-	int N;
-	cin >> N;
-
-	for (int i = 0; i < N; i++)
-	{
-		string s;
-		cin >> s;
-
-		cout << ((s.length() >= 6 && s.length() <= 9) ? "yes" : "no") << '\n';
-	}
-
-	return 0;
+    int T;
+    std::cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        int N;
+        std::cin >> N;
+        int reversed_N = reverse_number(N);
+        int sum = N + reversed_N;
+        if (is_palindrome(sum))
+        {
+            std::cout << "YES" << std::endl;
+        }
+        else
+        {
+            std::cout << "NO" << std::endl;
+        }
+    }
+    return 0;
 }
